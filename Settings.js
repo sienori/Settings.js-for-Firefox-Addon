01f930c150bc029bd04e312a42ece42d9c2df5b2
sienori <iilll7d6f@yahoo.co.jp>
2017-10-17 20:51:54 +0900

    Edit comment

--- Settings.js
+++ Settings.js
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 //初回起動時にオプションページを表示して設定を初期化
+//Display option page at initial startup and initialize settings
 browser.runtime.onInstalled.addListener(function(){
     browser.runtime.openOptionsPage();
 });
@@ -10,6 +11,7 @@ browser.runtime.onInstalled.addListener(function(){
 (function () {
     function settingsObj() {};
     //オプションページを書き換え，設定の初期化
+    //Rewrite option page, initialize setting
     settingsObj.prototype.initOptionsPage = function () {
         return new Promise(function (resolve, reject) {
             labelSet();
@@ -22,6 +24,7 @@ browser.runtime.onInstalled.addListener(function(){
         })
     };
     //オプションページから設定を保存
+    //Save settings from options page
     settingsObj.prototype.saveOptionsPage = function () {
         return new Promise(function (resolve, reject) {
             getSettingsByHtml();
@@ -31,6 +34,7 @@ browser.runtime.onInstalled.addListener(function(){
         })
     };
     //設定を初期化
+    //Initialize setting
     settingsObj.prototype.init = function () {
         return new Promise(function (resolve, reject) {
             getSettings().then(function () {
@@ -39,10 +43,12 @@ browser.runtime.onInstalled.addListener(function(){
         })
     }
     //設定を返す
+    //return settings
     settingsObj.prototype.get = function () {
         return Settings;
     };
     //受け取ったオブジェクトを保存
+    //Save the received object
     settingsObj.prototype.save = function (settings) {
         return new Promise(function (resolve, reject) {
             for (let i in settings) {
@@ -54,6 +60,7 @@ browser.runtime.onInstalled.addListener(function(){
         })
     };
     //設定を削除
+    //Delete settings
     settingsObj.prototype.clear = function () {
         return new Promise(function (resolve, reject) {
             Settings = new settingsObj();
@@ -64,9 +71,10 @@ browser.runtime.onInstalled.addListener(function(){
     }
 
     let Settings = new settingsObj();
-    S = new settingsObj(); //外部から呼び出し
+    S = new settingsObj(); //外部から呼び出し Call from outside
 
     //spanのid，buttonのclassに"Label"が含まれるときi18nから値を取得して書き換え
+    //When "label" is included in span's id, button class Retrieve the value from i18n and rewrite it
     function labelSet() {
         let spans = document.getElementsByTagName("span");
         for (let i in spans) {
@@ -88,11 +96,12 @@ browser.runtime.onInstalled.addListener(function(){
         }
     }
 
-    //storageからSettingsの項目を取得して存在しない物を上書き
+    //storageからSettingsの項目を取得して存在しない物をSettingsに上書き
+    //Retrieve the Settings item from storage and overwrite Settings that do not exist
     function overRideSettingsByStorage() {
         return new Promise(function (resolve, reject) {
             browser.storage.local.get("Settings", function (value) {
-                //htmlから初期化した設定がストレージに既に存在すればそちらを使う
+                
                 for (let i in Settings) {
                     if (value.Settings!=undefined && value.Settings[i] != undefined) {
                         Settings[i] = value.Settings[i];
@@ -107,6 +116,7 @@ browser.runtime.onInstalled.addListener(function(){
     }
 
     //オプションページにSettingsを反映
+    //Reflect Settings on option page
     function overRideHtml() {
         let inputs = document.getElementsByTagName("input");
         for (let i in inputs) {
@@ -140,7 +150,8 @@ browser.runtime.onInstalled.addListener(function(){
         }
     }
 
-    //オプションページから設定の値を取得 初期値はhtmlのvalue,checkedの状態
+    //オプションページから設定の値を取得
+    //Get setting value from option page
     function getSettingsByHtml() {
         let inputs = document.getElementsByTagName("input");
 
